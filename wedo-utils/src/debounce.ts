047db133b39e08fccb64759916a1e1aaa2e0f055
aiuluna <604401072@qq.com>
2023-04-19 02:18:43 +0800

    feat: 防抖函数

---
+++ wedo-utils/src/debounce.ts
@@ -0,0 +1,31 @@
+declare type FN = (...args: Array<any>) => void
+
+/**
+ * 防抖函数
+ * @param fn 
+ * @param delay 
+ * @param immediate 立即执行标志
+ * @returns 
+ */
+export function debounce(fn: FN, delay = 13, immediate = false) {
+  let timeId: number | undefined;
+  return function (this: any, ...args: Array<any>) {
+    const context = this;
+    const callNow = immediate && !timeId;
+
+    if (timeId) {
+      clearTimeout(timeId)
+    }
+
+    timeId = setTimeout(() => {
+      timeId = undefined;
+      if (!callNow) {
+        fn.apply(context, args)
+      }
+    }, delay);
+
+    if (callNow) {
+      fn.apply(context, args)
+    }
+  }
+}
