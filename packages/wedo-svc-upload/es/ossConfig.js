202391d3092f8b9f1514d039cb9ca9e1b7587584
xww <604401072@qq.com>
2023-07-03 15:58:48 +0800

    Delete ossConfig.js
    delete key

--- packages/wedo-svc-upload/es/ossConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    region: 'oss-cn-hangzhou',
-    accessKeyId: 'LTAI1RUO1pQJvDeC',
-    accessKeySecret: 'AfrJr1XmxAatyTlF8A479OGmjTRSux',
-    bucket: 'huzhang-wedo'
-};
-exports.default = config;
